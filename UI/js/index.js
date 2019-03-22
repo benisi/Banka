6bb5ec22c711d0dc82b6026b72baecd002acb9ea
benisi <oseikwemeisidahomen@gmail.com>
2019-03-22 15:04:22 +0100

    Add bank account profile

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,17 +1,25 @@
 // toggle button class
 
-document.querySelector(".toggle-btn").addEventListener('click', (e) => {
-    e.preventDefault();
+document.querySelector(".toggle-btn").addEventListener("click", ( event ) => {
+    event.preventDefault();
     const toggleIcon = document.querySelector(".toggle-btn").children[0];
-    document.querySelector(".side-bar").classList.toggle("hidden");
+    const sideBar = document.querySelector(".side-bar");
+    sideBar.classList.toggle("remove-sidebar");
     const containBarsIcon = toggleIcon.classList.contains("fa-bars");
-    if(containBarsIcon){
+    if (containBarsIcon) {
         toggleIcon.classList.remove("fa-bars");
-        toggleIcon.classList.add("fa-times");
-        toggleIcon.classList.add("turn");
-    }else{
-        toggleIcon.classList.remove("fa-times");
-        toggleIcon.classList.remove("turn");
+        toggleIcon.classList.add("fa-times", "turn");
+    } else {
+        toggleIcon.classList.remove("fa-times", "turn");
         toggleIcon.classList.add("fa-bars");
     }
-})
+});
+
+window.addEventListener("scroll", () => {
+    const topBar = document.querySelector(".top-bar");
+    if (window.scrollY > 10){
+        topBar.classList.add("floating-nav");
+    } else {
+        topBar.classList.remove("floating-nav");
+    }
+});
