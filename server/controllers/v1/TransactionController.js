06573e77202b734a5069f18df9d38361d9b49cf9
benisi <oseikwemeisidahomen@gmail.com>
2019-04-09 20:28:38 +0100

    Add base transaction controller

---
+++ server/controllers/v1/TransactionController.js
@@ -0,0 +1,12 @@
+class TransactionController {
+  static credit(req, res) {
+    return res.status(200).json({
+      status: 200,
+      data: {
+
+      }
+    });
+  }
+}
+
+export default TransactionController;
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserController from '../controllers/v1/UserController';
 import AccountController from '../controllers/v1/AccountController';
+import TransactionController from '../controllers/v1/TransactionController';
 import userValidator from '../middleware/userValidator';
 import accountValidator from '../middleware/accountValidator';
 import auth from '../helper/auth';
@@ -17,5 +18,8 @@ router.post('/api/v1/auth/signin', userValidator.signIn, UserController.signIn);
 router.post('/api/v1/accounts', [auth.verifyToken, accountValidator.create], AccountController.create);
 router.patch('/api/v1/account/:accountNumber', [auth.verifyToken, auth.allowOnlyAdminStaff, accountValidator.status], AccountController.status);
 router.delete('/api/v1/accounts/:accountNumber', [auth.verifyToken, auth.allowOnlyAdminStaff, accountValidator.delete], AccountController.delete);
+
+// Transaction route
+router.post('/api/v1/transactions/:accountNumber/credit', [auth.verifyToken], TransactionController.credit);
 
 export default router;
