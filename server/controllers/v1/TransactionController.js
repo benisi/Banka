0a3787da9961e177e2dd1b6f362f3b4c66597ad2
benisi <oseikwemeisidahomen@gmail.com>
2019-04-10 01:12:53 +0100

    Add controller

--- server/controllers/v1/TransactionController.js
+++ server/controllers/v1/TransactionController.js
@@ -33,7 +33,54 @@ class TransactionController {
       amount: transac.amount,
       cashier,
       transactionType: transac.type,
-      accountBalance: transac.newBalance.toString()
+      accountBalance: transac.newBalance.toFixed(2).toString()
+    };
+
+    return res.status(200).json({
+      status: 200,
+      data
+    });
+  }
+
+  static debit(req, res) {
+    const { accountData, amount } = req.body;
+    const accNumber = parseInt(accountData.accountNumber, 10);
+    const id = parseInt(req.body.decoded, 10);
+    const oldBalance = parseFloat(accountData.balance);
+    if (oldBalance < parseFloat(amount)) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Insufficient fund'
+      });
+    }
+    accountData.balance -= parseFloat(amount);
+    const transactionData = {
+      createdOn: new Date(),
+      type: 'debit',
+      accountNumber: accNumber,
+      cashier: id,
+      amount,
+      oldBalance,
+      newBalance: accountData.balance
+    };
+
+    const transac = transaction.create(transactionData);
+    if (!transac) {
+      return res.status(500).json({
+        status: 500,
+        error: 'Failed to credit account'
+      });
+    }
+
+    const { cashier } = transac;
+
+    const data = {
+      transactionId: transac.id,
+      accountNumber: transac.accountNumber.toString(),
+      amount: transac.amount,
+      cashier,
+      transactionType: transac.type,
+      accountBalance: transac.newBalance.toFixed(2).toString()
     };
 
     return res.status(200).json({
