e82a655c3c5e9e8bc2292969cdc882d591ad9442
benisi <oseikwemeisidahomen@gmail.com>
2019-04-22 18:15:43 +0100

    Feat: Admin can view all user account
    - Add function to setup database automatically
    
    [Delivers #165492034]

--- server/database/databaseSetup.js
+++ server/database/databaseSetup.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+
 
 dotenv.config();
 
@@ -24,16 +26,7 @@ const createUsersTableQuery = `CREATE TABLE IF NOT EXISTS users (
   password CHARACTER VARYING(255) NOT NULL
 )`;
 
-const createUserTable = () => {
-  pool.query(createUsersTableQuery)
-    .then(() => {
-      pool.end();
-    })
-    .catch((error) => {
-      pool.end();
-      throw error;
-    });
-};
+const createUserTable = () => pool.query(createUsersTableQuery);
 
 const createAccountTableQuery = `CREATE TABLE IF NOT EXISTS accounts(
   id BIGSERIAL PRIMARY KEY NOT NULL,
@@ -43,34 +36,18 @@ const createAccountTableQuery = `CREATE TABLE IF NOT EXISTS accounts(
   balance FLOAT(2) NOT NULL,
   category CHARACTER VARYING(13) NOT NULL,
   createdOn TIMESTAMP NOT NULL DEFAULT NOW(),
+  owner INTEGER NOT NULL,
   FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE
 )`;
 
-const createAccountTable = () => {
-  pool.query(createAccountTableQuery)
-    .then(() => {
-      pool.end();
-    })
-    .catch((error) => {
-      pool.end();
-      throw error;
-    });
-};
+const createAccountTable = () => pool.query(createAccountTableQuery);
 
 const createAccountTrackingTableQuery = `CREATE TABLE IF NOT EXISTS trackings (
   id BIGSERIAL PRIMARY KEY NOT NULL,
   type CHARACTER VARYING(7) NOT NULL
 )`;
 
-const createAccountTrackingTable = () => {
-  pool.query(createAccountTrackingTableQuery).then(() => {
-    pool.end();
-  })
-    .catch((error) => {
-      pool.end();
-      throw new Error(error);
-    });
-};
+const createAccountTrackingTable = () => pool.query(createAccountTrackingTableQuery);
 
 const createTransactionTableQuery = `CREATE TABLE IF NOT EXISTS transactions (
   id BIGSERIAL PRIMARY KEY NOT NULL,
@@ -85,20 +62,45 @@ const createTransactionTableQuery = `CREATE TABLE IF NOT EXISTS transactions (
   FOREIGN KEY (cashier) REFERENCES users (id)
 )`;
 
-const createTransactionTable = () => {
-  pool.query(createTransactionTableQuery)
+const createTransactionTable = () => pool.query(createTransactionTableQuery);
+const defaultAdmin = [
+  'admin@gmail.com',
+  'Benjamin',
+  'Isidahomen',
+  bcrypt.hashSync('adminpass1', 10),
+  'edo',
+  '07035361846',
+  'sir',
+  '02/09/1960',
+  'male',
+  'staff',
+  true,
+];
+const createAdminUserQuery = 'INSERT INTO users(email,firstname,lastname,password,stateofresidence,phonenumber,title, dateofbirth, sex, type, isadmin) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)';
+
+const createAdminUser = () => pool.query(createAdminUserQuery, defaultAdmin);
+
+const createAllTables = () => {
+  createUserTable()
+    .then(() => createAccountTable())
+    .then(() => createAccountTrackingTable())
+    .then(() => createTransactionTable())
     .then(() => {
-      pool.end();
-    })
-    .catch((error) => {
-      pool.end();
-      throw error;
+      createAdminUser().then(() => {
+        pool.end();
+      }).catch((err) => {
+        pool.end();
+        throw new Error(err);
+      });
     });
 };
 
-
 module.exports = {
-  createUserTable, createAccountTable, createTransactionTable, createAccountTrackingTable,
+  createUserTable,
+  createAccountTable,
+  createTransactionTable,
+  createAccountTrackingTable,
+  createAllTables,
 };
 
 require('make-runnable');
